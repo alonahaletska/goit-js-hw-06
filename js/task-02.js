1ac0928c1398e7068ec02adee2886fffb3ef3824
alonahaletska <al.khaletskaya@gmail.com>
2023-08-21 12:15:08 +0300

    add content

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const elemEl = [];
+
+ingredients.forEach((element) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = element;
+  elemEl.push(itemEl);
+});
+ingredientsEl.append(...elemEl);
